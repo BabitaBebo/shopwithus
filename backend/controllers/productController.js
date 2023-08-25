d26408070c137b6b0545be2a1bd44786149398cb
babita <babita565@gmail.com>
2023-08-25 23:14:21 +0530

    package.json

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -5,7 +5,7 @@ import Product from "../models/productModel.js";
 const getProducts = asyncHandler(async (req, res) => {
   // const products = await Product.find({});
   // res.json(products);
-  const pageSize = 4;
+  const pageSize = process.env.PAGINATION_LIMIT;
   const page = Number(req.query.pageNumber) || 1;
 
   const keyword = req.query.keyword
